41e076f2400a51ba683ec0dbf9c0d51ea2bbe9bf
Georden Canary <geordy0422@aol.com>
2017-08-31 12:28:27 -0400

    Fixed Layout of Project to Better understand everything

--- Graphics2Project/Graphics2Project.cpp
+++ Graphics2Project/Graphics2Project.cpp
@@ -6,38 +6,45 @@
 
 #define MAX_LOADSTRING 100
 
+//----------------------------------------------------------------------------------------------------------
 // Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+//----------------------------------------------------------------------------------------------------------
+HINSTANCE						hInst;                                    // current instance
+WCHAR							szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR							szWindowClass[MAX_LOADSTRING];            // the main window class name
+ID3D11Device*					m_pDevice				= nullptr;
+ID3D11DeviceContext*			m_pDeviceContext		= nullptr;
+IDXGISwapChain*					m_pSwapChain			= nullptr;
+ID3D11RenderTargetView*			m_pRenderTargetView 	= nullptr;
+//ID3D11Debug*					debug					= nullptr;
 
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------
 // Forward declarations of functions included in this code module:
+//----------------------------------------------------------------------------------------------------------
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+
+
+
+
+//----------------------------------------------------------------------------------------------------------
+// Entry point to the program. Initializes everything and goes into a message processing 
+// loop. Idle time is used to render the scene.
+//----------------------------------------------------------------------------------------------------------
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: Place code here.
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     // Initialize global strings
@@ -70,48 +77,47 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 
 
-//
+//----------------------------------------------------------------------------------------------------------
 //  FUNCTION: MyRegisterClass()
 //
-//  PURPOSE: Registers the window class.
-//
+//  PURPOSE: Registers the windows class.
+//----------------------------------------------------------------------------------------------------------
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GRAPHICS2PROJECT));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_GRAPHICS2PROJECT);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style                     = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc               = WndProc;
+    wcex.cbClsExtra                = 0;
+    wcex.cbWndExtra                = 0;
+    wcex.hInstance                 = hInstance;
+    wcex.hIcon                     = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GRAPHICS2PROJECT));
+    wcex.hCursor                   = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground             = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName              = MAKEINTRESOURCEW(IDC_GRAPHICS2PROJECT);
+    wcex.lpszClassName             = szWindowClass;
+    wcex.hIconSm                   = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
 }
 
-//
+//----------------------------------------------------------------------------------------------------------
 //   FUNCTION: InitInstance(HINSTANCE, int)
 //
-//   PURPOSE: Saves instance handle and creates main window
+//   PURPOSE: Saves instance handle and  ----- Creates main window
 //
 //   COMMENTS:
-//
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
-//
+//----------------------------------------------------------------------------------------------------------
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+      CW_USEDEFAULT, NULL, CW_USEDEFAULT, NULL, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
@@ -124,7 +130,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
+//----------------------------------------------------------------------------------------------------------
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
 //  PURPOSE:  Processes messages for the main window.
@@ -133,7 +139,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_PAINT    - Paint the main window
 //  WM_DESTROY  - post a quit message and return
 //
-//
+//----------------------------------------------------------------------------------------------------------
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -172,7 +178,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
+
+//--------------------------------------------------------------------------------------
+// Called every time the application receives a message
 // Message handler for about box.
+//--------------------------------------------------------------------------------------
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
